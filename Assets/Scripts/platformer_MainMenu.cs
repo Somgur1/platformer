4a85b3be858b23f1c98c797fa3e31bfe35d134a1
Somgur1 <Vincedewit@live.nl>
2023-06-06 13:50:28 +0200

    Added rollaball

---
+++ Assets/Scripts/platformer_MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class platformer_MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("MiniGame");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
